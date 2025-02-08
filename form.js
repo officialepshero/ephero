21d745d01e6c5eb9acf4d0339753e7edc439230b
officialepshero <officialepshero@gmail.com>
2025-02-08 18:49:39 +0630

    Update form.js

--- form.js
+++ form.js
@@ -107,6 +107,34 @@ function generateFormHTML() {
 <link rel="stylesheet" href="question-form.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+    <script language=JavaScript>
+<!--
+var message="Function Disabled!";
+///////////////////////////////////
+function clickIE4(){
+if (event.button==2){
+alert(message);
+return false;
+}
+}
+function clickNS4(e){
+if (document.layers||document.getElementById&&!document.all){
+if (e.which==2||e.which==3){
+alert(message);
+return false;
+}
+}
+}
+if (document.layers){
+document.captureEvents(Event.MOUSEDOWN);
+document.onmousedown=clickNS4;
+}
+else if (document.all&&!document.getElementById){
+document.onmousedown=clickIE4;
+}
+document.oncontextmenu=new Function("alert(message);return false")
+// ->
+</script>
 <body>
 <div class="logo">
         <img src="header.jpg" alt="Logo">
